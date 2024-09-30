d6db98b3849c1336ea88f7b7a60920dff423db1b
rezki bimo <rezkibimo@users.noreply.github.com>
2024-09-30 23:18:28 +1000

    styling
    added styling using bootstrap 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,40 +4,49 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <title>Home</title>
 
 </head>
 
 <body>
-    <h1> Car List </h1>
+    <div class="container">
+        <h1> Car List </h1>
 
-    @if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+        @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @if(!empty($cars))
+            @foreach($cars as $car)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $car['make'] }} {{ $car['model'] }}</h5>
+                    <p class="card-text">{{ $car['year'] }} - {{ $car['colour'] }}</p>
+                    <p class="card-text">{{ $car['licenseState'] }} - {{ $car['licensePlate'] }}</p>
+                    <form action="{{ url('/quotes') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="licenseState" value="{{ $car['licenseState'] }}">
+                        <input type="hidden" name="licensePlate" value="{{ $car['licensePlate'] }}">
+                        <button type="submit" class="btn btn-primary">Get Quote</button>
+                    </form>
+                </div>
+            </div>
             @endforeach
-        </ul>
-    </div>
-    @endif
+        </div>
 
-    @if(!empty($cars))
-    @foreach($cars as $car)
-    <div>
-        <p><strong>{{ $car['make'] }} - {{ $car['model'] }} - {{ $car['licensePlate'] }} - {{ $car['licenseState'] }} - {{ $car['year'] }} - {{ $car['colour'] }}</strong></p>
-        <form action="{{ url('/quotes') }}" method="POST">
-            @csrf
-            <input type="hidden" name="licensePlate" value="{{ $car['licensePlate'] }}">
-            <input type="hidden" name="licenseState" value="{{ $car['licenseState'] }}">
-            <button type="submit">Get Quote</button>
-        </form>
+        @else
+        <p>No cars found.</p>
+        @endif
     </div>
-    @endforeach
-    @else
-    <p>No cars found.</p>
-    @endif
-
 </body>
 
 </html>
--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <title>Car Quotes</title>
 
 </head>
 
-<body>
+<body class="container">
     <h1>Car Quotes</h1>
 
     @if ($errors->any())
@@ -23,17 +24,23 @@
     @endif
 
     @if(!empty($quotes))
-    <ul>
-        @foreach($quotes as $quote)
-        <li>{{ $quote['repairer'] }} - ${{ $quote['price'] }}</li>
-        <!-- Adjust based on the actual structure of your quotes data -->
-        @endforeach
-    </ul>
+    @foreach($quotes as $quote)
+    <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">{{ $quote['repairer'] }}</h4>
+        </div>
+        <div class="card-body">
+            <h1 class="card-title pricing-card-title">${{ $quote['price'] }}</h1>
+            <p class="card-text">{{ $quote['overviewOfWork'] }}</p>
+        </div>
+    </div>
+    <!-- Adjust based on the actual structure of your quotes data -->
+    @endforeach
     @else
     <p>No quotes found.</p>
     @endif
 
-    <a href="{{ url('/') }}">Back to Car List</a>
+    <a href="{{ url('/') }}">Back to My Car List</a>
 </body>
 
 </html>
